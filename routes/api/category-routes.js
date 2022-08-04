b90054a6a46bce9233cbfd8b2c78491ab86d6598
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-08-04 16:11:37 -0500

    fix routes w await but not async function

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoriesData = await Category.findAll({
+    const categoriesData = Category.findAll({
       include: [{ model: Product }]
     });
     res.status(200).json(categoriesData);
@@ -20,7 +20,7 @@ router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const singleCategoryData = await Category.findByPk(req.params.id, {
+    const singleCategoryData = Category.findByPk(req.params.id, {
       include: [{ model: Product }]
     });
 
@@ -62,7 +62,7 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
   try {
-    const singleCategoryData = await Category.destroy({
+    const singleCategoryData = Category.destroy({
       where: {
         id: req.params.id
       }
